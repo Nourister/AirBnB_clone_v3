c69cc13a1d43f1a40588e118cea90513db08514d
nourister <nouristerjuma@gmail.com>
2024-02-03 13:19:39 +0000

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -21,11 +21,11 @@ def stats():
     Create an endpoint that retrieves the number of each objects by type
     """
     stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User'),
     }
     return jsonify(stats)
